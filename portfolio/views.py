703a6942911055ce52112801a78e677c0cc0bfd7
Kyle Machulis <kyle@nonpolynomial.com>
2010-05-11 22:06:52 -0700

    Replaced internal template references with djhtml extensions

--- portfolio/views.py
+++ portfolio/views.py
@@ -19,7 +19,7 @@ def index(request):
         #if os.path.isfile(content_file):
         project_content_list.append(project.short_name)
     project_year_list = [ (k,project_year_list[k]) for k in sorted(project_year_list.keys(), reverse=True)]
-    return render_to_response('portfolio/index.html', {"project_year_list" : project_year_list, "project_content_list" : project_content_list})
+    return render_to_response('portfolio/index.djhtml', {"project_year_list" : project_year_list, "project_content_list" : project_content_list})
 
 def project(request, project_name):
     project = Project.objects.get(short_name=project_name)
@@ -52,7 +52,7 @@ def project(request, project_name):
         if photo.flickr_url is not None:
             photo.photo_url = photo.flickr_url
             photo.thumbnail_url = re.sub(r'.jpg', r'_t.jpg', photo.flickr_url)
-    return render_to_response('portfolio/project.html', {"project" : project, "desc": description, "press_links": press_links, "media_links": media_links, "photo_links": photo_links, "photo_thumbnails": photo_thumbnails, "related_projects": related_projects, "articles": articles,  "content_body": content_body })
+    return render_to_response('portfolio/project.djhtml', {"project" : project, "desc": description, "press_links": press_links, "media_links": media_links, "photo_links": photo_links, "photo_thumbnails": photo_thumbnails, "related_projects": related_projects, "articles": articles,  "content_body": content_body })
 
 def article(request, project_name, article_name):
     project = Project.objects.get(short_name=project_name)
@@ -61,5 +61,5 @@ def article(request, project_name, article_name):
     if os.path.isfile(article_path):
         with open(article_path, "r") as f:
             content_body = markdown.markdown(f.read())
-    return render_to_response('portfolio/article.html', {"project" : project, "content_body": content_body })
+    return render_to_response('portfolio/article.djhtml', {"project" : project, "content_body": content_body })
 
